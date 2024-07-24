e61f8bec8416540ca2c09e2ea1cd50c9ce4062c8
CTos-github <115353163+CTos-github@users.noreply.github.com>
2024-07-25 00:21:01 +0800

    delete

--- oh-my-live2d-main/oh-my-live2d-main/packages/oh-my-live2d/src/index.ts
+++
@@ -1,8 +0,0 @@
-import './library/iconfont.js';
-import './library/cubism2.js';
-import './library/cubism5.js';
-
-export { loadOml2d } from './modules/index.js';
-
-export type * from './types/options/index.js';
-export type * from './types/oml2d/index.js';
